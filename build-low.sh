ea1274aba8ec9d6b4445440e1a3df2e0896e7d3b
Adam J. Richardson <fatman.uk@gmail.com>
2022-04-22 18:05:30 +0100

    Update scripts' style

--- build-low.sh
+++ build-low.sh
@@ -1,10 +1,7 @@
-#!/bin/env bash
-set -o errexit
-set -o nounset
-set -o pipefail
+#!/usr/bin/env bash
+set -euo pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 DEV_HOSTS=${DEV_HOSTS:-localhost}
 PLAYBOOK=${PLAYBOOK:-$(readlink -f "${DIR}/books/build.yml")}
 
