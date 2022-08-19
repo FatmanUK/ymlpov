b1d05ecc9ff39ed4eae01234ec756463b988016c
Adam J. Richardson <fatman.uk@gmail.com>
2022-08-19 23:38:20 +0100

    Add script flag -epic= to render only specified model

--- build-high.sh
+++ build-high.sh
@@ -7,8 +7,21 @@ PLAYBOOK=${PLAYBOOK:-$(readlink -f "${DIR}/books/build.yml")}
 
 export ANSIBLE_INVENTORY="${ANSIBLE_INVENTORY:-$(readlink -f "${DIR}/ventory")}"
 
+ANSIBLE_ARGS=''
+POVRAY_ARGS=''
+
+while (( "$#" )); do
+        if [[ "-e" == "${1:0:2}" ]]; then
+                ANSIBLE_ARGS="${ANSIBLE_ARGS} $1"
+        else
+                POVRAY_ARGS="${POVRAY_ARGS} $1"
+        fi
+        shift
+done
+
 ansible-playbook \
 -e "qual='high'" \
--e "extra='$@'" \
+-e "extra='${POVRAY_ARGS}'" \
+${ANSIBLE_ARGS} \
 -l "${DEV_HOSTS}" \
 "${PLAYBOOK}"
